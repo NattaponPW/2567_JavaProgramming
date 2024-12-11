2fea5a6a3d95420fdd1cee76d30e9d6c105f186c
NattaponPW <nattapon2005768@gmail.com>
2024-12-11 11:20:59 +0700

    LabSheet03Practice

---
+++ LabSheet03/src/Lab303.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Lab303 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		
+		String txtMassage = "";
+		
+		System.out.print("Input a sentence : ");
+		String massage = input.nextLine();
+		
+	}
+
+}
