e36a4c4943af145f267a37c5a4d4e677ef40135b
NattaponPW <nattapon2005768@gmail.com>
2024-12-18 10:02:58 +0700

    LabSheet03Homework

--- LabSheet03/src/Lab303.java
+++ LabSheet03/src/Lab303.java
@@ -6,10 +6,30 @@ public class Lab303 {
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner(System.in);
 		
-		String txtMassage = "";
-		
 		System.out.print("Input a sentence : ");
 		String massage = input.nextLine();
+		
+		
+		while (!massage.endsWith(".")) {
+			System.out.print("Input a setence, again : ");
+			massage = input.nextLine();
+		}
+		
+		int spaceCount = 0;
+		
+		for (int c = 0; c < massage.length(); c++ ) {
+			if (massage.charAt(c) == ' ') {
+				spaceCount++;
+			}
+			 //System.out.println(massage.charAt(c)); // Test Code
+		}
+		
+		int word = spaceCount + 1;
+        
+        System.out.println("This sentence has " + spaceCount + " spacebar.");
+        System.out.println("This sentence has " + word + " word.");
+     
+        input.close();
 		
 	}
 
